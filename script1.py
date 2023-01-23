1f1fe89be51c3b7c91ca3eef36302a7e5abc9b69
Jaxpat1054 <122818508+Jaxpat1054@users.noreply.github.com>
2023-01-23 13:21:08 -0500

    Deleted
    Deleted a print from both scripts.

--- script1.py
+++ script1.py
@@ -1,4 +1,3 @@
 #3 sentence printed
 print("Explicit is better than implicit.")
 print("Simple is better than complex.")
-print("Complex is better than complicated.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
 print("Now is better than never.")
 print("Although never is often better than *right * now.")    
-print("If the implementation is hard to explain, it's a bad idea.")
